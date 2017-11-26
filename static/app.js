4a3571baf55c342f0204d6759028edfe407145a5
Lee Jung Woo <lmwljw98@naver.com>
2017-11-26 22:04:20 +0900

    send

---
+++ static/app.js
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script>
+
+    var photo = "{{ my }}";
+
+    $(document).scroll(function () {
+
+        var maxHeight = $(document).height();
+        var currentScroll = $(window).scrollTop() + $(window).height();
+
+        if (maxHeight <= currentScroll + 100) {
+            $("body").append('<div class="big-box"><h1>Page{{ my.1 }}</h1></div>');
+            $("body").append('<div class="big-box"><h1>Page{{ my.0 }}</h1></div>');
+        }
+    });
+</script>
