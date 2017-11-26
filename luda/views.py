4ccd4e17ca169bd58d47d34e1b46f6d72afa641e
Lee Jung Woo <lmwljw98@naver.com>
2017-11-26 19:35:33 +0900

    send

--- luda/views.py
+++ luda/views.py
@@ -6,4 +6,4 @@ import os
 
 def test(request):
     ret_list = os.listdir("./static/images/")
-    return render(request, 'luda/main.html', {'list': ret_list})
+    return render(request, 'luda/main.html', {'my': ret_list})
