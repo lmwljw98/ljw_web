b784b9adb2fb817d435ccdfe2395f642c86db780
Lee Jung Woo <lmwljw98@naver.com>
2017-11-26 20:19:04 +0900

    send

--- luda/views.py
+++ luda/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 import os
-
+import json
 
 # Create your views here.
 
 def test(request):
     ret_list = os.listdir("./static/images/")
-    return render(request, 'luda/main.html', {'images': ret_list})
+    json_list = json.dump(ret_list)
+    return render(request, 'luda/main.html', {'images': json_list})
