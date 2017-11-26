eaea8c869807c50a751191f09064b633111709f6
Lee Jung Woo <lmwljw98@naver.com>
2017-11-26 22:25:35 +0900

    send

--- luda/models.py
+++ luda/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+
+class My(models.Model):
+    image_name = models.CharField(max_length=9999, default="")
--- luda/views.py
+++ luda/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 import os
 import json
+from luda.models import My
 
 
 # Create your views here.
 
 def test(request):
     ret_list = os.listdir("./static/images/")
-    return render(request, 'luda/main.html', {'my': ['a', 'b', 'c']})
+    for i in range(len(ret_list)):
+        # My.objects.create(image_name=ret_list[i])
+        q = My(image_name=ret_list[i])
+        q.save()
+
+    return render(request, 'luda/main.html', {'my': My.objects.all()})
