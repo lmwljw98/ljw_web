2888258632df4898cb04a6488a650dcebc803a85
Lee Jung Woo <lmwljw98@naver.com>
2017-11-26 16:52:33 +0900

    send

--- luda/views.py
+++ luda/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import os
+
 
 # Create your views here.
 
 def test(request):
-    return render(request, 'luda/main.html',)
+    return render(request, 'luda/main.html', {'list': os.listdir("./static/images/")})
