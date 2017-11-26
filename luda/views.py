ae42682a831c8730e727767d7dd3f83250e6ca45
Lee Jung Woo <lmwljw98@naver.com>
2017-11-26 18:17:15 +0900

    send

--- luda/views.py
+++ luda/views.py
@@ -5,5 +5,5 @@ import os
 # Create your views here.
 
 def test(request):
-    ret_list = os.listdir("./static/images/")
-    return render(request, 'luda/main.html', {'list': ret_list})
+    # ret_list = os.listdir("./static/images/")
+    return render(request, 'luda/main.html')
