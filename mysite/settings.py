68a14f78155e6cf2c8e0a44557034a3660fc051f
Lee Jung Woo <lmwljw98@naver.com>
2017-11-30 08:56:26 +0900

    setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
