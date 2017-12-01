ee52332df68cc099436b9a3a768795fbaec69606
Lee Jung Woo <lmwljw98@naver.com>
2017-12-01 18:32:58 +0900

    1201

--- luda/views.py
+++ luda/views.py
@@ -9,16 +9,22 @@ from luda.models import My
 
 def test(request):
     entry_list = list(My.objects.values_list('image_name', flat=True))
+    entry_list2 = list(My.objects.values_list('gif_name', flat=True))
     # for i in range(len(entry_list)):
     #    entry_list[i] = entry_list[i].replace("&#39;", "'")
     random.shuffle(entry_list)
-    return render(request, 'luda/main.html', {'my': entry_list})
+    random.shuffle(entry_list2)
+    return render(request, 'luda/main.html', {'my': entry_list, 'my2': entry_list2})
 
 
 def db(request):
     My.objects.all().delete()
     ret_list = os.listdir("./static/images/")
+    ret_list2 = os.listdir("./static/images/gif")
     for i in range(len(ret_list)):
-        q = My(image_name=ret_list[i])
+        q = My(image_name=ret_list2[i])
+        q.save()
+    for i in range(len(ret_list2)):
+        q = My(gif_name=ret_list2[i])
         q.save()
     return render(request, 'luda/main.html')
