3c2977837ac536256a62b1c1c146b00e8f5301b2
Lee Jung Woo <lmwljw98@naver.com>
2017-12-16 16:42:58 +0900

    vod

--- vodbot/views.py
+++ vodbot/views.py
@@ -3,87 +3,58 @@ import json, os
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
-mediaCode_list = []
-mediaStr_list = []
+final_url = []
 base_url = "http://cjpiporigin.myskcdn.com/VOD/"
 
-
-def searchMediaCode(keyword):
-    mediaCode_list.clear()
-    mediaStr_list.clear()
-    params = {'kwd': keyword, 'pageSize': 50}
-    mediaCode_request = requests.get('http://search.tving.com:8080/search/getFind.jsp', params=params)
-    mediaCode = json.loads(mediaCode_request.text)
-
-    for i in range(len(mediaCode['vodBCRsb']['dataList'])):
-        mediaStr_list.append(str(i) + " = " + mediaCode['vodBCRsb']['dataList'][i]['epi_nm'])
-        mediaCode_list.append(mediaCode['vodBCRsb']['dataList'][i]['epi_cd'])
-    myStr = "\n".join(mediaStr_list)
-
-    return myStr
+proxies = {
+    "https": "https://211.58.248.163:3128"
+}
 
 
-def searchProgramCode(index):
-    k = mediaCode_list[index]
-    os.system("sudo python3 ./vodbot/search.py %s &" % k)
+def searchMediaCode(name):
+    try:
+        keyword = name
+        final_url.clear()
 
+        params = {'kwd': keyword, 'pageSize': 1000}
+        mediaCode_request = requests.get('http://search.tving.com:8080/search/getFind.jsp', params=params,
+                                         proxies=proxies)
+        mediaCode = json.loads(mediaCode_request.text)
 
-def text(request):
-    return JsonResponse(
-        {
-            'type': 'text'
-        }
-    )
+        for i in range(len(mediaCode['vodBCRsb']['dataList'])):
+
+            second_params = {'mediaCode': mediaCode['vodBCRsb']['dataList'][i]['epi_cd'], 'info': 'Y'}
+            programCode_request = requests.get('http://api.tving.com/v1/media/stream/info', params=second_params,
+                                               proxies=proxies)
+            programCode = json.loads(programCode_request.text)
+
+            realCode = programCode['body']['content']['info']['program']['enm_code']
+            fre_number = mediaCode['vodBCRsb']['dataList'][i]['frequency']
+
+            final_url.append(mediaCode['vodBCRsb']['dataList'][i]['mast_nm'].replace("#@$", "").replace("$#@", "") + " "
+                + mediaCode['vodBCRsb']['dataList'][i]['frequency'] + "화\n" + base_url + realCode + "/" + realCode +
+                             "_" + fre_number + ".mp4")
+
+    except:
+        return "Error"
 
 
 @csrf_exempt
 def message(request):
-    str = ((request.body).decode('utf-8'))
-    received = json.loads(str)
+    user_request = ((request.body).decode('utf-8'))
+    received = json.loads(user_request)
     name = received['content']
 
-    if name.isdigit():
-        number = int(name)
-        searchProgramCode(number)
+    searchMediaCode(name)
 
-        return JsonResponse(
-            {
-                'message': {
-                    'text':
-                        "잠시 대기한 뒤(약 5초간) 'ok [보고 싶은 화]'를 입력해주세요.\n\nEX) ok 7 -> 입력 시 7화"
-                },
-                'keyboard': {
-                    'type': 'text'
-                }
+    return JsonResponse(
+        {
+            'message': {
+                'text':
+                    "\n".join(final_url)
+            },
+            'keyboard': {
+                'type': 'text'
             }
-        )
-    elif name.split()[0] == "ok":
-        f = open("result.txt", "r")
-        code = f.readline()
-        f.close()
-
-        return JsonResponse(
-            {
-                'message': {
-                    'text': base_url + code + "/" + code + "_" + name.split()[1] + ".mp4"
-
-                },
-                'keyboard': {
-                    'type': 'text'
-                }
-            }
-        )
-
-    elif not name.isdigit():
-        my = searchMediaCode(name)
-        return JsonResponse(
-            {
-                'message': {
-                    'text': my + "\n\n원하는 프로그램의 번호를 입력하세요."
-
-                },
-                'keyboard': {
-                    'type': 'text'
-                }
-            }
-        )
+        }
+    )
